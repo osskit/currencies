e4b2e71a148947a99701dcd9e30c498f0c6fa487
Sharon Grossman <sharong1337@gmail.com>
2023-02-22 14:52:44 +0200

    fix

--- src/currencies.ts
+++ src/currencies.ts
@@ -1,6 +1,5 @@
-import type { CurrencyCode } from './types.js';
 import { data } from './data.js';
 
-export const getByCode = (code: CurrencyCode) => data.find(({ code: currentCode }) => currentCode === code);
+export const getByCode = (code: string) => data.find(({ code: currentCode }) => currentCode === code);
 export const getByNumber = (number: string) => data.find(({ number: currentNumber }) => currentNumber === number);
 export const getCodes = () => data.map(({ code }) => code);
