f65dd338e24394797cde13947ec819df6b159480
Noam Okman <noamokman@gmail.com>
2023-03-21 15:29:13 +0200

    add is currency code

--- src/currencies.ts
+++ src/currencies.ts
@@ -1,5 +1,8 @@
 import { data } from './data.js';
+import type { CurrencyCode } from './types.js';
 
 export const getByCode = (code: string) => data.find(({ code: currentCode }) => currentCode === code);
 export const getByNumber = (number: string) => data.find(({ number: currentNumber }) => currentNumber === number);
 export const getCodes = () => data.map(({ code }) => code);
+export const isCurrencyCode = (code: unknown): code is CurrencyCode =>
+  typeof code === 'string' && getCodes().includes(code as CurrencyCode);
