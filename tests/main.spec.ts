6edfa33e51908443e52ba61ecb437c7004e42c2c
Noam Okman <noamokman@gmail.com>
2023-03-21 16:42:53 +0200

    add is currency code (#4)

--- tests/main.spec.ts
+++ tests/main.spec.ts
@@ -1,4 +1,4 @@
-import { data, getByCode, getByNumber, getCodes } from '../src/index.js';
+import { data, getByCode, getByNumber, getCodes, isCurrencyCode } from '../src/index.js';
 
 describe('currencies', () => {
   describe('exports', () => {
@@ -16,6 +16,128 @@ describe('currencies', () => {
 
     it('should expose a get codes', () => {
       expect(typeof getCodes).toBe('function');
+    });
+
+    it('should expose is currency code', () => {
+      expect(typeof isCurrencyCode).toBe('function');
+    });
+  });
+
+  describe('getByCode', () => {
+    it('should return the currency', () => {
+      expect(getByCode('EUR')).toMatchInlineSnapshot(`
+        {
+          "code": "EUR",
+          "countries": [
+            "Åland Islands",
+            "Andorra",
+            "Austria",
+            "Belgium",
+            "Croatia",
+            "Cyprus",
+            "Estonia",
+            "European Union",
+            "Finland",
+            "France",
+            "French Guiana",
+            "French Southern Territories (The)",
+            "Germany",
+            "Greece",
+            "Guadeloupe",
+            "Holy See (The)",
+            "Ireland",
+            "Italy",
+            "Latvia",
+            "Lithuania",
+            "Luxembourg",
+            "Malta",
+            "Martinique",
+            "Mayotte",
+            "Monaco",
+            "Montenegro",
+            "Netherlands (The)",
+            "Portugal",
+            "Réunion",
+            "Saint Barthélemy",
+            "Saint Martin (French Part)",
+            "Saint Pierre and Miquelon",
+            "San Marino",
+            "Slovakia",
+            "Slovenia",
+            "Spain",
+          ],
+          "currency": "Euro",
+          "digits": 2,
+          "number": "978",
+        }
+      `);
+    });
+  });
+
+  describe('getByNumber', () => {
+    it('should return the currency', () => {
+      expect(getByNumber('978')).toMatchInlineSnapshot(`
+        {
+          "code": "EUR",
+          "countries": [
+            "Åland Islands",
+            "Andorra",
+            "Austria",
+            "Belgium",
+            "Croatia",
+            "Cyprus",
+            "Estonia",
+            "European Union",
+            "Finland",
+            "France",
+            "French Guiana",
+            "French Southern Territories (The)",
+            "Germany",
+            "Greece",
+            "Guadeloupe",
+            "Holy See (The)",
+            "Ireland",
+            "Italy",
+            "Latvia",
+            "Lithuania",
+            "Luxembourg",
+            "Malta",
+            "Martinique",
+            "Mayotte",
+            "Monaco",
+            "Montenegro",
+            "Netherlands (The)",
+            "Portugal",
+            "Réunion",
+            "Saint Barthélemy",
+            "Saint Martin (French Part)",
+            "Saint Pierre and Miquelon",
+            "San Marino",
+            "Slovakia",
+            "Slovenia",
+            "Spain",
+          ],
+          "currency": "Euro",
+          "digits": 2,
+          "number": "978",
+        }
+      `);
+    });
+  });
+
+  describe('getCodes', () => {
+    it('should return the codes', () => {
+      expect(getCodes()).toHaveLength(179);
+    });
+  });
+
+  describe('isCurrencyCode', () => {
+    it('should return true for valid currency code', () => {
+      expect(isCurrencyCode('EUR')).toBe(true);
+    });
+
+    it('should return false for invalid currency code', () => {
+      expect(isCurrencyCode('EURO')).toBe(false);
     });
   });
 });
