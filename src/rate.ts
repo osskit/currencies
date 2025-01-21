e79cf43d0cfbb97f6a54f0d9fc318ff94960d649
Daniel Witz <37773232+danielwitz@users.noreply.github.com>
2025-01-21 15:11:01 +0200

    handle floating point correctly (#10)

--- src/rate.ts
+++ src/rate.ts
@@ -1,9 +1,11 @@
 import type { Amount } from './types.js';
 import { toMajorUnit } from './conversion.js';
 
+const JS_BUFFER_MULTIPLIER = 10_000;
+
 export const calculateRate = (from: Amount, to: Amount) => {
   const fromInMajor = toMajorUnit(from);
   const toInMajor = toMajorUnit(to);
 
-  return toInMajor / fromInMajor;
+  return (JS_BUFFER_MULTIPLIER * toInMajor) / (JS_BUFFER_MULTIPLIER * fromInMajor);
 };
